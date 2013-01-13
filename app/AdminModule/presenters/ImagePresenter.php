cbbf1fd88568240226eaa7dbfdb8c1968f5b1365
Jan Mochnak <janmochnak@gmail.com>
2013-01-13 09:51:46 +0100

    remove imagine from old uploading

--- app/AdminModule/presenters/ImagePresenter.php
+++ app/AdminModule/presenters/ImagePresenter.php
@@ -2,13 +2,13 @@
 
 namespace AdminModule;
 
-require LIBS_DIR.'/imagine-v0.3.0.phar';
+//require LIBS_DIR.'/imagine-v0.3.0.phar';
 
-use Nette\Application\Responses\JsonResponse; 
+use Nette\Application\Responses\JsonResponse;
 use Nette\Image;
 
 class ImagePresenter extends \BasePresenter {
-	
+
 	public function actionUpload()
 	{
 		$file = $this->getHttpRequest()->getFile('file');
@@ -18,21 +18,13 @@ class ImagePresenter extends \BasePresenter {
 		$path = 'uploads/photos';
 		$image->save(WWW_DIR.'/'.$path.'/'.$baseFileName, 100);
 		unset($image);
-		// $image->resize(210, 230);
-		// $image->save(WWW_DIR.'/'.$path.'/t_'.$baseFileName, 100);
-				
-
-		$imagine = new \Imagine\Gd\Imagine();
-		$transformation = new \Imagine\Filter\Transformation();
-		$transformation->thumbnail(new \Imagine\Image\Box(210, 230), \Imagine\Image\ImageInterface::THUMBNAIL_OUTBOUND);
-		$transformation->apply($imagine->open(WWW_DIR.'/'.$path.'/'.$baseFileName))
-        	->save(WWW_DIR.'/'.$path.'/t_'.$baseFileName);
-
+		$image->resize(210, 230);
+		$image->save(WWW_DIR.'/'.$path.'/t_'.$baseFileName, 100);
 
 		$url = "http://spse-po.sk/newsletter/system/";
 
 		$res = new JsonResponse(array ( 'filelink' => $url.$path.'/'.$baseFileName, 'thumb' => $url.$path.'/t_'.$baseFileName) );
-		$this->sendResponse($res); 
+		$this->sendResponse($res);
 	}
 
 }
